53de8dded8257b3099c480f36d256e46cde54f86
Chris Shepherd <thesheps@gmail.com>
2019-02-16 13:34:08 +0000

    Removing some of the vanillaness

---
+++ src/components/Tracker.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hi</div>
+</template>
+
+<script>
+export default {
+  name: "trackr",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Tracker/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Tracker from "@/components/Tracker.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Tracker
   }
 };
 </script>
